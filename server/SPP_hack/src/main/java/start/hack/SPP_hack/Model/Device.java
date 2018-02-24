a565697dca4e8772194baeeb8a4ef6c4e0252181
Hamza <151425@umons.ac.be>
2018-02-24 14:05:01 +0100

    Correction Database 2

---
+++ server/SPP_hack/src/main/java/start/hack/SPP_hack/Model/Device.java
@@ -0,0 +1,37 @@
+package start.hack.SPP_hack.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="`device`")
+public class Device
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="`indexDevice`", length= 20)
+    private int indexDevice;
+
+    // Clé étrangère
+    @OneToOne
+    @JoinColumn(name="`indexSensor`")
+    private Sensor indexSensor;
+
+    public Sensor getIndexSensor() {
+        return indexSensor;
+    }
+
+    public void setIndexSensor(Sensor indexSensor) {
+        this.indexSensor = indexSensor;
+    }
+    ///
+
+    public int getIndexDevice() {
+        return indexDevice;
+    }
+
+    public void setIndexDevice(int indexDevice) {
+        this.indexDevice = indexDevice;
+    }
+
+
+}
