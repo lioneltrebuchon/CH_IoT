eb954e154edf9b3415e5d0fddb7ce22a737efc98
Hamza <151425@umons.ac.be>
2018-02-24 10:56:36 +0100

    Correction to controller again7

--- server/SPP_hack/src/main/java/start/hack/SPP_hack/Controller/RequestController.java
+++ server/SPP_hack/src/main/java/start/hack/SPP_hack/Controller/RequestController.java
@@ -5,19 +5,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import start.hack.SPP_hack.Model.Data;
 
 @RestController
 @RequestMapping("/rest")
 public class RequestController {
-    @RequestMapping(method = RequestMethod.POST,path="/")
-    public void insert(@RequestBody String sObj){
+    @RequestMapping(method = RequestMethod.GET,path="/{obj}")
+    public void insert(@PathVariable("obj") String sObj){
 		JSONObject obj = new JSONObject(sObj);
 		System.out.println("Dans fct"); 
         System.out.println(obj.toString());
     }
 	@RequestMapping(path="/get")
-	public void test(){
+	public void test(@RequestBody String sObj){
+		JSONObject obj = new JSONObject(sObj);
 		System.out.println("Test get");
 	}
     @RequestMapping(method = RequestMethod.POST,path="/request")
